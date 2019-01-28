3c0a5cb65728bbcc05e9e7afcc39e8957fa782a7
evangu <727668704@qq.com>
2019-01-28 15:32:45 +0800

    jdk8新特性

--- src/main/java/learn/java/jdk8/Jdk8NewAbility.java
+++ src/main/java/learn/java/jdk8/Jdk8NewAbility.java
@@ -55,7 +55,9 @@ public class Jdk8NewAbility {
 
     /**
      *  {@link java.util.Optional}
-     * Java 8引入Optional类来防止空指针异常，Optional类最先是由Google的Guava项目引入的。Optional类实际上是个容器：它可以保存类型T的值，或者保存null。使用Optional类我们就不用显式进行空指针检查了。
+     * Java 8引入Optional类来防止空指针异常，Optional类最先是由Google的Guava项目引入的。
+     * Optional类实际上是个容器：它可以保存类型T的值，或者保存null。
+     * 使用Optional类我们就不用显式进行空指针检查了。
      *
      * /
 
